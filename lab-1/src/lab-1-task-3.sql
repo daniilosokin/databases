b05a5bbdfea6d9fa3e8f4d1abec30f71a419de7d
daniilosokin <d.d.osokin@mail.ru>
2025-09-22 16:47:18 +0400

    style: made code more readable

--- lab-1/src/lab-1-task-3.sql
+++ lab-1/src/lab-1-task-3.sql
@@ -1,34 +1,43 @@
--- Вывод всех предметов.
-SELECT `name` from `exam_session`.`subject`;
+/* ======================= ЗАДАНИЕ 1 ======================= */
+SELECT name 
+  FROM exam_session.subject;
 
--- Вывод количества студентов.
-SELECT count(`student_id`) as `student_count` FROM `exam_session`.`student`;
+/* ======================= ЗАДАНИЕ 2 ======================= */
+SELECT COUNT(student_id) AS student_count 
+  FROM exam_session.student;
 
--- Вывод студентов, чья фамилия начинается на «Ива».
-SELECT * FROM `exam_session`.`student`
-	WHERE `surname` LIKE "Ива%";
+/* ======================= ЗАДАНИЕ 3 ======================= */
+SELECT * 
+  FROM exam_session.student
+ WHERE surname LIKE "Ива%";
 
--- Вывод студентов, родившихся позже указанной даты.
-SELECT * FROM `exam_session`.`student` 
-	WHERE `birthdate` > '2000-06-30';
+/* ======================= ЗАДАНИЕ 4 ======================= */
+SELECT * 
+  FROM exam_session.student 
+ WHERE birthdate > '2000-06-30';
 
--- Вывод студентов, получивших оценки 5 по указанному предмету.
-SELECT `student`.* FROM `exam_session`.`student`
-	JOIN `exam_session`.`grade` ON `grade`.`student_id` = `student`.`student_id`
-	JOIN `exam_session`.`subject` ON `grade`.`subject_id` = `subject`.`subject_id`
-	WHERE `grade`.`grade` = 5 and `subject`.`subject_id` = 1;
+/* ======================= ЗАДАНИЕ 5 ======================= */
+SELECT student.* 
+  FROM exam_session.student
+	   JOIN exam_session.grade 
+	     ON grade.student_id = student.student_id
+	   JOIN exam_session.subject 
+	     ON grade.subject_id = subject.subject_id
+ WHERE grade.grade = 5 
+   AND subject.subject_id = 1;
 
--- Вывод студентов, получивших оценки только 4 и 5 
--- по всем предметам в указанном семестре,
--- упорядочить по фамилии
-SELECT `student`.* FROM `exam_session`.`student`
-	JOIN `exam_session`.`grade` ON `grade`.`student_id` = `student`.`student_id`
-	JOIN `exam_session`.`subject` ON `grade`.`subject_id` = `subject`.`subject_id`
-	WHERE `term` = 1 AND (`grade` = 4 or `grade` = 5)
-	GROUP BY `student_id`
-	HAVING COUNT(*) = (
-		SELECT COUNT(*) 
-			FROM `exam_session`.`subject` 
-			WHERE `term` = 1
-	)
-	ORDER BY `surname` ASC;
+/* ======================= ЗАДАНИЕ 6 ======================= */
+SELECT student.* 
+  FROM exam_session.student
+	   JOIN exam_session.grade 
+	     ON grade.student_id = student.student_id
+	   JOIN exam_session.subject
+         ON grade.subject_id = subject.subject_id
+ WHERE term = 1 
+   AND grade IN (4, 5)
+ GROUP BY student_id
+HAVING COUNT(*) = 
+       (SELECT COUNT(*) 
+		  FROM exam_session.subject 
+		 WHERE term = 1)
+ ORDER BY surname ASC;
