43981ec1d0d2b448e13da61ca9105e7bd3aaa545
daniilosokin <d.d.osokin@mail.ru>
2025-09-22 21:32:40 +0400

    lab-3: added query 3

--- lab-3/src/lab-3-query.sql
+++ lab-3/src/lab-3-query.sql
@@ -1,9 +1,9 @@
 /* =========================== ЗАПРОС 1 ============================ */
 SELECT model_title,
-       COUNT(model.model_id) as count
+       COUNT(model.model_id) AS count
   FROM bicycles
 	   JOIN model
-		 ON bicycles.model_id = model.model_id
+		 ON model.model_id = bicycles.model_id
  GROUP BY model.model_id;
  
 /* =========================== ЗАПРОС 2 ============================ */
@@ -11,10 +11,22 @@ SELECT DISTINCT point_id,
        point_title
   FROM rental
        JOIN rental_points
-         ON rental.pick_point_id = rental_points.point_id
+         ON rental_points.pick_point_id = rental.point_id
 	   JOIN bicycles
-		 ON rental.bicycle_id = bicycles.bicycle_id
+		 ON bicycles.bicycle_id = rental.bicycle_id
 	   JOIN model
-         ON bicycles.model_id = model.model_id
+         ON model.model_id = bicycles.model_id
  WHERE model_title LIKE 'Bianchi Oltre'
  ORDER BY point_title;
+ 
+/* =========================== ЗАПРОС 3 ============================ */
+SELECT model_title,
+       SUM(TIMESTAMPDIFF(HOUR, start_time, end_time)) AS duration
+  FROM bicycles
+	   JOIN model
+		 ON model.model_id = bicycles.model_id
+	   JOIN rental
+		 ON rental.bicycle_id = bicycles.bicycle_id
+ WHERE end_time IS NOT NULL
+ GROUP BY model_title
+ ORDER BY duration;
