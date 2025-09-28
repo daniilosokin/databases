f66ae2ef708969ac71f77e690ba88547a8544e2e
daniilosokin <d.d.osokin@mail.ru>
2025-09-28 14:01:36 +0400

    feat(lab-4): added task 1

---
+++ lab-4/src/lab-4.sql
@@ -0,0 +1,8 @@
+/* =========================== ЗАПРОС 1 ============================ */
+CREATE VIEW non_updated_view AS
+     SELECT model_title,
+            COUNT(bicycle_id) AS count
+       FROM models
+            LEFT JOIN bicycles
+		    ON bicycles.model_id = models.model_id
+      GROUP BY model_title;
