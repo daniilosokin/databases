b768f2612274aaa895cb9ecdda3463c94cb82923
daniilosokin <d.d.osokin@mail.ru>
2025-09-28 14:20:27 +0400

    feat(lab-4): added task 2

--- lab-4/src/lab-4.sql
+++ lab-4/src/lab-4.sql
@@ -6,3 +6,9 @@ CREATE VIEW non_updated_view AS
             LEFT JOIN bicycles
 		    ON bicycles.model_id = models.model_id
       GROUP BY model_title;
+      
+/* =========================== ЗАДАНИЕ 2 ============================ */
+CREATE VIEW updated_view AS
+     SELECT point_id,
+            point_title
+       FROM rental_points;
