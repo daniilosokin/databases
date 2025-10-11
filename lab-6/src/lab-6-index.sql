38d0992389cb81b1366b9759d4e00a227a61854e
daniilosokin <d.d.osokin@mail.ru>
2025-10-11 18:01:22 +0400

    feat: added index 2

--- lab-6/src/lab-6-index.sql
+++ lab-6/src/lab-6-index.sql
@@ -15,3 +15,38 @@ FROM
     discogs.artist
 WHERE 
     NAME = "Пикник";
+    
+/* =========================== ИНДЕКСЫ 2 ============================ */
+CREATE INDEX 
+    group_main_artist_id_index 
+ON 
+    discogs.group(MAIN_ARTIST_ID);
+    
+CREATE INDEX 
+    group_group_artist_id_index 
+ON 
+    discogs.group(GROUP_ARTIST_ID);
+    
+ALTER TABLE 
+    discogs.group 
+DROP INDEX 
+    group_main_artist_id_index,
+DROP INDEX 
+    group_group_artist_id_index;
+
+SELECT 
+    * 
+FROM 
+    discogs.artist
+WHERE 
+    discogs.artist.ARTIST_ID IN (
+        SELECT 
+            GROUP_ARTIST_ID 
+        FROM 
+            discogs.group
+        JOIN 
+            discogs.artist 
+            ON discogs.artist.ARTIST_ID = discogs.group.MAIN_ARTIST_ID
+        WHERE 
+            discogs.artist.NAME = "Пикник"
+    );
